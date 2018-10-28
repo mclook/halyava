6acc8786a51534db7eaba6cac4fcee099160101c
mclook <44475053+mclook@users.noreply.github.com>
2018-10-28 20:49:19 +0200

    initial

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot=require('node-telegram-bot-api')
 const TOKEN='798561841:AAHCxxSkrco-deXJg5K2wuA1hr-o5PxhufI'
 
 const bot=new TelegramBot(TOKEN,{polling:true})
-
+bot.get_updates(allowed_updates=["channel_post"])
 bot.onText(/message123/,msg=>{
     bot.sendMessage(msg.chat.id,'/message123@alcopawerbot')
 })
