e7b7ef8d0354cadd20f1c9971eb793e0722b8a52
mclook <44475053+mclook@users.noreply.github.com>
2018-10-28 21:37:57 +0200

    initial

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const TOKEN='798561841:AAHCxxSkrco-deXJg5K2wuA1hr-o5PxhufI'
 
 const bot=new TelegramBot(TOKEN,{polling:true})
 
-bot.on('message', function (msg) {
+bot.onText(/message123/,msg=>{
     bot.sendMessage(msg.chat.id,'/message123@alcopawerbot')
-});
+})
 require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
     res.end('')
 })
