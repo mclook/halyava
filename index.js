9e62a127f8b3f347c5a7fb3a113129ddff1bb7d8
mclook <44475053+mclook@users.noreply.github.com>
2018-12-03 22:16:27 +0200

    initial

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const TelegramBot=require('node-telegram-bot-api')
 const TOKEN='798561841:AAHCxxSkrco-deXJg5K2wuA1hr-o5PxhufI'
 
 const bot=new TelegramBot(TOKEN,{polling:true})
-
+const chatMax='452867599';
 bot.onText(/message123/,msg=>{
     bot.sendMessage(msg.chat.id,'/message123@alcopawerbot')
 })
-require('http').createServer().listen(process.env.PORT || 5000).on('request', function(req, res){
-    res.end('')
+bot.on('message',function(msg){
+    bot.sendMessage(msg.chat.id,'/message123@alcopawerbot')
+    bot.forwardMessage(chatMax,chat.id,message_Id)
+
+
+
 })
+
+
+
 
