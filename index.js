f685c6f4a21c98f549c0f20d9935f76af6f14f09
mclook <44475053+mclook@users.noreply.github.com>
2018-12-03 22:57:01 +0200

    initial

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const TOKEN='798561841:AAHCxxSkrco-deXJg5K2wuA1hr-o5PxhufI'
 const bot=new TelegramBot(TOKEN,{polling:true})
 const chatMax='452867599';
 bot.onText(/message123/,msg=>{
-    bot.sendMessage(msg.chat.id,'/message123@alcopawerbot')
+    bot.sendMessage(msg.chat.id,'/message123@alcopawerbot');
 })
 bot.on('message',msg=>{
     const {chat:{id}}=msg;
-   bot.sendMessage(chatMax,id)
-    bot.sendMessage(chatMax,id.message.message_id)
+
+   bot.sendMessage(chatMax,id);
+    bot.sendMessage(chatMax,id.message.text);
     //message.message_id
    // const {message:{message_id}}=msg;
     bot.forwardMessage(chatMax,chatMax,id.message.message_id)
