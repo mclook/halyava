1e637c1425c3e2e290e737a2ee9c60a2fa21b260
mclook <44475053+mclook@users.noreply.github.com>
2018-12-03 23:14:12 +0200

    initial

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ bot.on('message',msg=>{
    const {chat:{id}}=msg;
    // const{message:{chat,message_id,text}}=msg;
    bot.sendMessage(chatMax,id);
-   // bot.sendMessage(chatMax,);
+    bot.sendMessage(chatMax,msg.message.message_id);
     //message.message_id
    // const {message:{message_id}}=msg;
     bot.forwardMessage(chatMax,id,msg.message.message_id);
